09680fa76e5faaf0a7a42fe41c803e65bd8b71c0
Shannon Code <genecyber@gmail.com>
2017-09-28 13:47:05 -0400

    git bundle

---
+++ archive/bundle.sh
@@ -0,0 +1,9 @@
+git pull
+git checkout page-8
+git checkout page-13
+git checkout page-17
+git checkout page-23
+git checkout page-42
+git checkout page-74
+git checkout page-0
+git bundle create ./archive.repo --all
